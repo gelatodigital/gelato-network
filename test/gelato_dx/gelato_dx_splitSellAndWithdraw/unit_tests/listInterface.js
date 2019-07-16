cbeaad1ef30dc8149a3d251f1bf896c0aca46d9d
bytezantium <bytezantium@gmail.com>
2019-07-16 17:40:34 +0200

    truffle test repo restructured

--- test/gelato_dx/gelato_dx_splitSellAndWithdraw/unit_tests/listInterface.js
+++ test/gelato_dx/gelato_dx_splitSellAndWithdraw/unit_tests/listInterface.js
@@ -1,7 +1,7 @@
 // Script to list the GelatoDXSplitSellAndWithdraw Interface on Gelato Core
 
 // Module Imports
-const assert = require('assert');
+const assert = require("assert");
 
 // Truffle Artifacts
 const GelatoCore = artifacts.require("GelatoCore");
@@ -14,18 +14,25 @@ MAXGAS = 400000; // 400.000
 
 // Set before Each
 let accounts;
-let gelatoCoreOwner;
 let gelatoCore;
 let gelatoDXSplitSellAndWithdraw;
+let gelatoCoreOwner;
+let gelatoDXSplitSellAndWithdrawOwner;
 
 // fetch accounts, Core and Interface contracts before each test
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
-  gelatoCoreOwner = accounts[0];
+
+  // Fetch gelatoCore and gelatoInterface from blockchain
   gelatoCore = await GelatoCore.at(GelatoCore.address);
   gelatoDXSplitSellAndWithdraw = await GelatoDXSplitSellAndWithdraw.at(
     GelatoDXSplitSellAndWithdraw.address
   );
+
+  // Check that latters' owners are accounts[0] (as per 3_deploy_gelato.js)
+  gelatoCoreOwner = await GelatoCore.owner().call();
+  gelatoDXSplitSellAndWithdrawOwner = await gelatoDXSplitSellAndWithdrawOwner.methods.owner().call();
+  assert.equal(gelatoCoreOwner, gelatoDXSplitSellAndWithdrawOwner, accounts[0]);
 });
 
 describe("gelatoCore.listInterface(_dappInterface, _maxGas) onlyOwner", () => {
@@ -43,10 +50,10 @@ describe("gelatoCore.listInterface(_dappInterface, _maxGas) onlyOwner", () => {
 
     let interface = await gelatoCore.interfaceWhitelist(
       gelatoDXSplitSellAndWithdraw.address
-    );  // boolean
+    ); // boolean
     let maxGas = await gelatoCore.maxGasByInterface(
       gelatoDXSplitSellAndWithdraw.address
-    );  // uint256
+    ); // uint256
 
     assert(interface);
     assert.equal(MAXGAS, maxGas);
