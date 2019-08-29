ed5677f7e5946108cf91277dd422bdc069992429
gitpusha <gitpusha@gmail.com>
2019-08-29 09:56:26 +0200

    migrations edits

---
+++ migrations/4_deploy_gelatoCore.js
@@ -0,0 +1,45 @@
+/* global artifacts */
+/* eslint no-undef: "error" */
+
+// Set Gelato Contract as truffle artifact
+const GelatoCore = artifacts.require("GelatoCore");
+
+// GelatoCore constructor params
+const MIN_INTERFACE_BALANCE = web3.utils.toWei("0.5", "ether");
+const EXECUTOR_PROFIT = web3.utils.toWei("2", "finney");
+const EXECUTOR_GAS_PRICE = web3.utils.toWei("5", "gwei");
+const EXEC_FN_GAS_OVERHEAD = 34034;
+const EXEC_FN_REFUNDED_GAS = 30000;
+const RECOMMENDED_GAS_PRICE_FOR_INTERFACES = web3.utils.toWei("100", "gwei");
+
+module.exports = async function(deployer, network, accounts) {
+  const coreDeployer = accounts[0];
+  // const _interfaceDeployer = accounts[1];
+
+  // Deploy GelatoCore with gelatoGasPrice
+  console.log(`
+        Deploying GelatoCore.sol with
+        =============================
+        Owner: ${coreDeployer}
+        executorProfit:    ${EXECUTOR_PROFIT}
+        executorGasPrice:  ${EXECUTOR_GAS_PRICE}
+        execFNGasOverhead: ${EXEC_FN_GAS_OVERHEAD}
+        execFNRefundedGas: ${EXEC_FN_REFUNDED_GAS}
+        `)
+  await deployer.deploy(
+    GelatoCore,
+    MIN_INTERFACE_BALANCE,
+    EXECUTOR_PROFIT,
+    EXECUTOR_GAS_PRICE,
+    EXEC_FN_GAS_OVERHEAD,
+    EXEC_FN_REFUNDED_GAS,
+    RECOMMENDED_GAS_PRICE_FOR_INTERFACES,
+    { from: coreDeployer }
+  );
+
+  const gelatoCore = await GelatoCore.deployed();
+  console.log(`
+        Deployed GelatoCore instance at:
+        ================================
+        ${gelatoCore.address}`);
+};
---
+++ migrations/5_deploy_gelatoDutchX.js
@@ -0,0 +1,44 @@
+/* global artifacts */
+/* eslint no-undef: "error" */
+
+// Set Gelato Contract as truffle artifact
+const GelatoCore = artifacts.require("GelatoCore");
+const GelatoDutchX = artifacts.require("GelatoDutchX");
+const DutchExchangeProxy = artifacts.require("DutchExchangeProxy");
+const INTERFACE_MAX_GAS = 500000; // sell and withdraw claims benchmarking
+const INTERFACE_GAS_PRICE = 0; // defaults to gelatoCore.recommendedGasPriceForInterface
+
+module.exports = async function(deployer, network, accounts) {
+  // const _coreDeployer = accounts[0];
+  const interfaceDeployer = accounts[0];
+
+  const gelatoCore = await GelatoCore.deployed();
+
+  // Deploy GelatoDutchX interface
+  // Make sure the proxy is deployed
+  const dxProxy = await DutchExchangeProxy.deployed();
+  console.log(`
+        Deploying GelatoDutchX.sol with
+        ================================================
+        Owner: ${interfaceDeployer}
+        GelatoCore: ${gelatoCore.address}
+        DutchXProxy: ${dxProxy.address}
+        interfaceMaxGas: ${INTERFACE_MAX_GAS}
+        interfaceGasPrice: ${INTERFACE_GAS_PRICE} (0 = gelatoCore default)`);
+  await deployer.deploy(
+    GelatoDutchX,
+    gelatoCore.address,
+    dxProxy.address,
+    INTERFACE_MAX_GAS,
+    INTERFACE_GAS_PRICE,
+    {
+      from: interfaceDeployer
+    }
+  );
+
+  const gelatoDutchX = await GelatoDutchX.deployed();
+  console.log(`
+        Deployed GelatoDutchX instance at:
+        ==================================================
+        ${gelatoDutchX.address}`);
+};
