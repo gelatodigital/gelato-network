540bac1a25508afc0987d32124b46e2d5ea00ff9
gitpusha <gitpusha@gmail.com>
2019-11-04 17:12:50 +0100

    added dependencies to package.json

--- networks.js
+++ networks.js
@@ -1,12 +1,28 @@
+require("dotenv").config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const DEV_MNEMONIC = process.env.DEV_MNEMONIC;
+const INFURA_ID = process.env.INFURA_ID;
+
 module.exports = {
   networks: {
     development: {
-      protocol: 'http',
-      host: 'localhost',
+      protocol: "http",
+      host: "localhost",
       port: 8545,
       gas: 5000000,
       gasPrice: 5e9,
-      networkId: '*',
+      networkId: "*"
     },
-  },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(DEV_MNEMONIC, "https://ropsten.infura.io/v3/" + INFURA_ID),
+      networkId: 3 // Ropsten's id
+    }/*,
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(DEV_MNEMONIC, "https://rinkeby.infura.io/v3/" + INFURA_ID),
+      networkId: 3 // Ropsten's id
+    }*/
+  }
 };
