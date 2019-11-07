da32872c31e872d30b41ef5ff9d3f2b3a37ba3e7
gitpusha <gitpusha@gmail.com>
2019-11-07 12:23:57 +0100

    TriggerDutchXAuctionCleared works and deployed

--- contracts/triggers_actions/triggers/dapp_triggers/dutchX_triggers/TriggerDutchXAuctionCleared.sol
+++ contracts/triggers_actions/triggers/dapp_triggers/dutchX_triggers/TriggerDutchXAuctionCleared.sol
@@ -1,58 +1,60 @@
-/*pragma solidity ^0.5.10;
+pragma solidity ^0.5.10;
 
-import '../../../../1_gelato_standards/3_GTA_standards/gelato_trigger_standards/GelatoTriggersStandard.sol';
-import '../../../../1_gelato_standards/1_gelato_dappInterface_standards/gelato_dutchX/IDutchX.sol';
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import '../../GelatoTriggersStandard.sol';
+import '../../../../interfaces/dapp_interfaces/dutchX_interfaces/IDutchX.sol';
 
-contract TriggerDutchXAuctionCleared is GelatoTriggersStandard {
+contract TriggerDutchXAuctionCleared is Initializable,
+                                        GelatoTriggersStandard
+
+{
 
     IDutchX public dutchX;
 
-    constructor(address payable _gelatoCore,
-                string memory _triggerSignature,
-                address _dutchX
-    )
-        public
-        GelatoTriggersStandard(_gelatoCore, _triggerSignature)
+    function initialize(address _dutchX)
+        external
+        initializer
     {
+        triggerSelector = this.fired.selector;
         dutchX = IDutchX(_dutchX);
+    }
+
+    function getDutchXAddress() external view returns(address) { return address(dutchX);}
+
+    function _getAuctionClosingPrices(address _sellToken,
+                                      address _buyToken,
+                                      uint256 _auctionIndex
+    )
+        internal
+        view
+        returns(uint256 num, uint256 den)
+    {
+        (num, den) = dutchX.closingPrices(_sellToken, _buyToken, _auctionIndex);
     }
 
     function getAuctionClosingPrices(address _sellToken,
                                      address _buyToken,
                                      uint256 _auctionIndex
     )
-        public
+        external
         view
-        returns(uint256 num, uint256 den)
+        returns(uint256, uint256)
     {
-        (num, den) = dutchX.closingPrices(_sellToken,
-                                          _buyToken,
-                                          _auctionIndex
-        );
+        return _getAuctionClosingPrices(_sellToken, _buyToken, _auctionIndex);
     }
 
     function fired(address _sellToken,
                    address _buyToken,
                    uint256 _auctionIndex
     )
-        public
+        external
         view
         returns(bool)
     {
-        (, uint256 den) = getAuctionClosingPrices(_sellToken, _buyToken, _auctionIndex);
+        (, uint256 den) = _getAuctionClosingPrices(_sellToken, _buyToken, _auctionIndex);
         if (den == 0) {
             return false;
-        } else {
-            return true;
         }
-    }
-
-    function getDutchXAddress()
-        public
-        view
-        returns(address)
-    {
-        return address(dutchX);
+        return true;
     }
 }
-*/
