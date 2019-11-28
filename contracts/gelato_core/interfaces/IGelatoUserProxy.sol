7ae43bacc5782a327c9c6ac67f54ad59385a7dec
gitpusha <gitpusha@gmail.com>
2019-11-28 15:00:30 +0100

    small fix

--- contracts/gelato_core/interfaces/IGelatoUserProxy.sol
+++ contracts/gelato_core/interfaces/IGelatoUserProxy.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.5.10;
 interface IGelatoUserProxy {
     function getUser() external view returns(address payable);
     function getGelatoCore() external view returns(address payable);
-    function setGelatoCore(address payable _gelatoCore) external;
     function execute(address payable _action, bytes calldata _actionPayloadWithSelector)
         external
         payable
