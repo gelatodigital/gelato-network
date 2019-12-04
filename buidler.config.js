6cd845d0e874fd81f4f788ca13f4ef40bbc70513
gitpusha <gitpusha@gmail.com>
2019-12-04 18:42:14 +0100

    progress save buidler tasks

--- buidler.config.js
+++ buidler.config.js
@@ -1,6 +1,5 @@
 // Local imports
-require("@babel/register");
-const { getDefaultProvider, providers } = require("ethers");
+const { Contract, getDefaultProvider, providers, utils } = require("ethers");
 
 // ============ Buidler Runtime Environment (BRE) ==================================
 // extendEnvironment(env => { env.x = x; })
@@ -32,7 +31,7 @@ usePlugin("@nomiclabs/buidler-ethers");
 // ============ Tasks ==============================================================
 // task action function receives the Buidler Runtime Environment as second argument
 task(
-  "blockNumber",
+  "block-number",
   "Logs the current block number of connected network",
   async (_, { ethers }) => {
     try {
@@ -49,7 +48,7 @@ task(
 );
 
 task(
-  "blockNumber:ropsten",
+  "block-number-ropsten",
   "Logs the current block number of Ropsten Test Net",
   async () => {
     try {
@@ -66,9 +65,86 @@ task(
   }
 );
 
-task("ether:balance");
+task("erc20-approve", "Approves <spender> for <erc20> <amount>")
+  .addParam("erc20", "The erc20 contract address")
+  .addParam("spender", "The spender's address")
+  .addParam("amount", "The amount of erc20 tokens to approve")
+  .setAction(async (taskArgs, { ethers }) => {
+    try {
+      const erc20Address = utils.getAddress(taskArgs.erc20);
+      const spenderAddress = utils.getAddress(taskArgs.spender);
+      const amount = utils.bigNumberify(taskArgs.amount);
+      const [signer, signer2, ...rest] = await ethers.signers();
+      console.log(await signer.getAddress());
+      console.log(await signer2.getAddress());
+      const ierc20ABI = [
+        "function approve(address spender, uint256 amount) external returns (bool)"
+      ];
+      /*const erc20Contract = await ethers.getContract(
+        erc20Address,
+        ierc20ABI,
+        signer
+      );*/
+      const erc20Contract = new Contract(
+        erc20Address,
+        ierc20ABI,
+        signer
+      );
+      const tx = await erc20Contract.approve(spenderAddress, amount);
+      console.log(`\n\t\t erc20:approve txHash:\n\t ${tx.hash}`);
+      const txReceipt = await tx.wait();
+      console.log(`\t\t Tx mined: ${txReceipt.blockNumber !== undefined}`);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
-task("ether:price", "Logs the etherscan ether-USD price", async () => {
+task("erc20-allowance", "Logs <spender>'s <erc20> allowance from <owner>")
+  .addParam("erc20", "The erc20 contract address")
+  .addParam("owner", "The owners's address")
+  .addParam("spender", "The spender's address")
+  .setAction(async taskArgs => {
+    try {
+      const erc20Address = utils.getAddress(taskArgs.erc20);
+      const ownerAddress = utils.getAddress(taskArgs.owner);
+      const spenderAddress = utils.getAddress(taskArgs.spender);
+      const provider = ethers.provider;
+      const wallet = Wallet.fromMnemonic(DEV_MNEMONIC);
+      const connectedWallet = wallet.connect(provider);
+      const ierc20ABI = [
+        "function allowance(address owner, address spender) external view returns (uint256)"
+      ];
+      const erc20Contract = new Contract(
+        erc20Address,
+        ierc20ABI,
+        connectedWallet
+      );
+      const allowance = await erc20Contract.allowance(
+        ownerAddress,
+        spenderAddress
+      );
+      console.log(
+        `\n\t\t erc20:allowance: ${utils.formatEther(allowance)} ETH`
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+task("eth-balance", "Prints an account's ether balance")
+  .addParam("a", "The account's address")
+  .setAction(async taskArgs => {
+    try {
+      const address = utils.getAddress(taskArgs.account);
+      const provider = getDefaultProvider("ropsten");
+      const balance = await provider.getBalance(address);
+      console.log(`\n\t\t ${utils.formatEther(balance)} ETH`);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+task("eth-price", "Logs the etherscan ether-USD price", async () => {
   try {
     const etherscanProvider = new providers.EtherscanProvider();
     const { name: networkName } = await etherscanProvider.getNetwork();
@@ -80,7 +156,7 @@ task("ether:price", "Logs the etherscan ether-USD price", async () => {
 });
 
 task(
-  "network:current",
+  "network-current",
   "Logs the currently connected network",
   async (_, { ethers }) => {
     try {
