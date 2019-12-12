4a5412cf1451254826fd25f701f5ecbe2f9cab02
gitpusha <gitpusha@gmail.com>
2019-12-12 09:16:33 +0100

    REMOVED old dutchx contracts and scripts

--- unused_contracts/unused_actions/IGelatoUpgradeableAction.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.14;
-
-interface IGelatoUpgradeableAction {
-    function askProxyForProxyAdminAddress() external view returns(address);
-    function askImplementationIfInit() external view returns(bool);
-    function askProxyIfImplementationInit() external view returns(bool);
-    function askProxyForImplementationAddress() external view returns(address);
-}
