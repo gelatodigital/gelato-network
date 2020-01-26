d1df5128c7b27563650cc8efbae9e1d085627263
gitpusha <gitpusha@gmail.com>
2020-01-26 17:26:28 +0100

    new luis userproxy

--- buidler.config.js
+++ buidler.config.js
@@ -100,7 +100,7 @@ module.exports = {
         },
         userProxy: {
           // Kovan
-          luis: "0x5392364b493ba4043C168b7a33906381B9255689"
+          luis: "0x217f828cA13d99C9bBaaf07fc95A9394d6BC795F"
         }
       },
       contracts: [
@@ -131,8 +131,6 @@ module.exports = {
         ActionKyberTradeKovan: "0xE5656d2dAAbF6a94F7B05315735D33193246289a",
         // ==== Gelato Core ===
         GelatoCore: "0xEFD00E5Ce7b62CA41190Da3818d2427f04427117",
-        // Luis User Proxy
-        GelatoUserProxy: "0x5392364b493ba4043C168b7a33906381B9255689",
         // === Conditions ===
         // balance
         ConditionBalance: "0xA5EA265F49A574BA5CfeEdB7cE5fc9E330DF1441",
