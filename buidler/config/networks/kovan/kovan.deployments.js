e32fab822b1a0035e7aeac920ec4a7ac768b2762
gitpusha <gitpusha@gmail.com>
2020-02-18 17:48:27 +0100

    GnosisSafeProxyEnableGelatoCore deployed

--- buidler/config/networks/kovan/kovan.deployments.js
+++ buidler/config/networks/kovan/kovan.deployments.js
@@ -17,5 +17,7 @@ export const deployments = {
   // kyber
   ConditionKyberRateKovan: "0xD8eBB69Dc566E86eA6e09A15EBe6Fd9c65c4A698",
   // time
-  ConditionTimestampPassed: "0x10A46c633adfe5a6719f3DBd2c162676779fE70B"
+  ConditionTimestampPassed: "0x10A46c633adfe5a6719f3DBd2c162676779fE70B",
+  // ==== Scripts ====
+  GnosisSafeProxyEnableGelatoCore: "0xf53f625aDE4d53905081cC390845a5f9C2EC137a"
 };
