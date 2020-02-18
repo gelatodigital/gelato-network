9d53521392b1e461c8fa0c24ba5125b7bf85466e
gitpusha <gitpusha@gmail.com>
2020-02-18 09:49:04 +0100

    updated gnosis safe proxy creation task with default values

--- buidler/tasks/gelato/core/gnosis_safe_proxy_manager/task.createGnosisSafeProxy.js
+++ buidler/tasks/gelato/core/gnosis_safe_proxy_manager/task.createGnosisSafeProxy.js
@@ -1,4 +1,4 @@
-import { task } from "@nomiclabs/buidler/config";
+import { task, types } from "@nomiclabs/buidler/config";
 import { defaultNetwork } from "../../../../../buidler.config";
 import { constants } from "ethers";
 
@@ -6,58 +6,100 @@ export default task(
   "gc-creategnosissafeproxy",
   `Sends tx to GelatoCore.createGnosisSafeProxy() on [--network] (default: ${defaultNetwork})`
 )
-  .addOptionalVariadicPositionalParam(
-    "initializerargs",
-    "args to be encoded into the initializer data"
-  )
   .addOptionalParam(
     "mastercopy",
     "The deployed implementation code the created proxy should point to"
   )
-  .addOptionalParam("initializer", "payload for gnosis safe proxy setup tasks")
+  .addOptionalParam("initializer", "Payload for gnosis safe proxy setup tasks")
+  .addFlag("setup", "Initialize gnosis safe by calling its setup function")
+  .addOptionalVariadicPositionalParam(
+    "owners",
+    "Supply with --setup: List of owners. Defaults to ethers signer."
+  )
+  .addOptionalParam(
+    "threshold",
+    "Supply with --setup: number of required confirmations for a Safe Tx.",
+    1,
+    types.int
+  )
+  .addOptionalParam(
+    "to",
+    "Supply with --setup: contract address for optional delegatecall.",
+    constants.AddressZero
+  )
+  .addOptionalParam(
+    "data",
+    "Supply with --setup: payload for optional delegate call",
+    "0x0"
+  )
+  .addOptionalParam(
+    "fallbackHandler",
+    "Supply with --setup:  Handler for fallback calls to this contract",
+    constants.AddressZero
+  )
+  .addOptionalParam(
+    "paymentToken",
+    "Supply with --setup:  Token that should be used for the payment (0 is ETH)",
+    constants.AddressZero
+  )
+  .addOptionalParam(
+    "payment",
+    "Supply with --setup:  Value that should be paid",
+    0,
+    types.int
+  )
+  .addOptionalParam(
+    "paymentReceiver",
+    "Supply with --setup:  Adddress that should receive the payment (or 0 if tx.origin)t",
+    constants.AddressZero
+  )
   .addFlag("log", "Logs return values to stdout")
-  .setAction(async ({ mastercopy, initializerargs, initializer, log }) => {
+  .setAction(async taskArgs => {
     try {
-      if (!mastercopy) {
-        mastercopy = await run("bre-config", {
+      if (!taskArgs.initializer && !taskArgs.setup)
+        throw new Error("Must provide initializer payload or --setup args");
+      else if (taskArgs.initializer && taskArgs.setup)
+        throw new Error("Provide EITHER initializer payload OR --setup args");
+
+      if (!taskArgs.mastercopy) {
+        taskArgs.mastercopy = await run("bre-config", {
           addressbookcategory: "gnosisSafe",
           addressbookentry: "mastercopy"
         });
       }
 
-      if (!mastercopy) throw new Error("No mastercopy for proxy defined");
+      if (!taskArgs.mastercopy)
+        throw new Error("No taskArgs.mastercopy for proxy defined");
 
-      if (log) console.log(`\n Mastercopy: ${mastercopy}\n`);
+      if (taskArgs.setup && !taskArgs.owners) {
+        const signer = await run("ethers", { signer: true, address: true });
+        taskArgs.owners = [signer];
+        if (!Array.isArray(taskArgs.owners))
+          throw new Error("Failed to convert taskArgs.owners into Array");
+      }
 
-      if (!initializerargs && !initializer)
-        throw new Error("Must provide initializer args or initializer payload");
+      if (taskArgs.log) console.log("\nTaskArgs:\n", taskArgs, "\n");
 
-      if (!initializer) {
-        if (!Array.isArray(initializerargs[0])) {
-          const tmp = initializerargs[0];
-          initializerargs[0] = [tmp];
-        }
-        if (!Array.isArray(initializerargs[0]))
-          throw new Error("Failed to convert initializerargs[0] into an Array");
-
-        for (const index in initializerargs) {
-          if (initializerargs[index] === "addresszero")
-            initializerargs[index] = constants.AddressZero;
-        }
-
-        if (log) {
-          console.log(`Initilizer Arguments:`);
-          for (const arg of initializerargs) console.log(typeof arg, arg);
-        }
-
-        initializer = await run("abi-encode-withselector", {
+      if (taskArgs.setup) {
+        const inputs = [
+          taskArgs.owners,
+          taskArgs.threshold,
+          taskArgs.to,
+          taskArgs.data,
+          taskArgs.fallbackHandler,
+          taskArgs.paymentToken,
+          taskArgs.payment,
+          taskArgs.paymentReceiver
+        ];
+        taskArgs.initializer = await run("abi-encode-withselector", {
           contractname: "IGnosisSafe",
           functionname: "setup",
-          inputs: initializerargs
+          inputs
         });
       }
 
-      if (log) console.log(`\nInitializer payload ${initializer}\n`);
+      if (taskArgs.log)
+        console.log(`\nInitializer payload:\n${taskArgs.initializer}\n`);
 
       const gelatoCoreContract = await run("instantiateContract", {
         contractname: "GelatoCore",
@@ -65,15 +107,16 @@ export default task(
       });
 
       const creationTx = await gelatoCoreContract.createGnosisSafeProxy(
-        mastercopy,
-        initializer
+        taskArgs.mastercopy,
+        taskArgs.initializer
       );
 
-      if (log) console.log(`\n\ntxHash createUserProxy: ${creationTx.hash}`);
+      if (taskArgs.log)
+        console.log(`\ntxHash createUserProxy: ${creationTx.hash}\n`);
 
       const { blockHash } = await creationTx.wait();
 
-      if (log) {
+      if (taskArgs.log) {
         const parsedLog = await run("event-getparsedlogs", {
           contractname: "GelatoCore",
           eventname: "LogGnosisSafeProxyUserCreation",
@@ -81,13 +124,15 @@ export default task(
           blockHash,
           values: true
         });
-        const { user, gnosisSafeProxy } = parsedLog[0];
+        const { user, gnosisSafeProxy } = parsedLog;
         console.log(
-          `\nLogGnosisSafeProxyCreation\nuser: ${user}\ngnosisSafeProxy: ${gnosisSafeProxy}\n`
+          `\n LogGnosisSafeProxyCreation\
+           \n User:            ${user}\
+           \n GnosisSafeProxy: ${gnosisSafeProxy}\n`
         );
       }
 
-      return tx.hash;
+      return creationTx.hash;
     } catch (error) {
       console.error(error);
       process.exit(1);
