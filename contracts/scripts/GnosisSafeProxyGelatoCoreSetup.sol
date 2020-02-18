56ecba9fa291c259e374cf2ce7376b008b599909
gitpusha <gitpusha@gmail.com>
2020-02-18 17:20:21 +0100

    progress save pre Major Refactor GelatoCore no prepayments no hardcoded gas

--- contracts/scripts/GnosisSafeProxyGelatoCoreSetup.sol
+++ contracts/scripts/GnosisSafeProxyGelatoCoreSetup.sol
@@ -2,12 +2,21 @@ pragma solidity ^0.6.2;
 
 import "../gelato_core/interfaces/IGelatoCore.sol";
 
+// For debugging purposes we do not revert if anything goes wrong
+//  so that we can emit the LogFailure event. This is necessary because the
+//  delegatecalling GnosisSafeProxy low-level catches reverts and overrides
+//  their message. see ModuleManager.setupModules require expression:
+// https://github.com/gnosis/safe-contracts/blob/aa0f3345b609a816ace6c448960ddb852b8a1bbd/contracts/base/ModuleManager.sol#L29
+
 /// @title CreateAndMint
-/// @notice Script to be delegatecalled during Gnosis Safe Proxy setup
-/// @dev gnosisSafeProxy.setup.setupModules(to,data:
-///       - <to> should be the address of GnosisSafeProxyGelatoCoreSetup
-///       - <data> should be the encodedPayload
+/// @notice Script to be run during Gnosis Safe Proxy setup for Gelato integration
+/// @dev Should be delegatecalled from gnosisSafeProxy.setup.setupModules(to,data):
+///       - <to> should be the address of this contract: GnosisSafeProxyGelatoCoreSetup
+///       - <data> should be the encodedPayload for whitelistAndMint
 contract GnosisSafeProxyGelatoCoreSetup {
+
+    event LogFailure(string error);
+
     /// @dev This function should be delegatecalled
     function whitelistAndMint(
         address _gelatoCore,
@@ -22,9 +31,9 @@ contract GnosisSafeProxyGelatoCoreSetup {
         // Whitelist GelatoCore as module on delegatecaller (Gnosis Safe Proxy)
         try IGnosisSafe(address(this)).enableModule(_gelatoCore) {
         } catch Error(string memory error) {
-            revert(error);
+            emit LogFailure(error);
         } catch {
-            revert("GnosisSafeProxyGelatoCoreSetup: undefined enableModule error");
+            emit LogFailure("enableModule error");
         }
 
         // Mint on GelatoCore from delegatecaller (Gnosis Safe Proxy)
@@ -34,11 +43,11 @@ contract GnosisSafeProxyGelatoCoreSetup {
             _conditionPayloadWithSelector,
             _action,
             _actionPayloadWithSelector
-        ) {
+        )  {
         } catch Error(string memory error) {
-            revert(error);
+            emit LogFailure(error);
         } catch {
-            revert("GnosisSafeProxyGelatoCoreSetup: undefined mintExecutionClaim error");
+            emit LogFailure("mintExecutionClaim error");
         }
     }
 }
