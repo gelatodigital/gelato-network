4df3c97d2c286b961310cf5aaa4e26e28b81a6cc
gitpusha <gitpusha@gmail.com>
2020-02-19 10:24:03 +0100

    Major: new version of GelatoCore deployed to Kovan

--- buidler/config/networks/kovan/kovan.deployments.js
+++ buidler/config/networks/kovan/kovan.deployments.js
@@ -10,7 +10,7 @@ export const deployments = {
   // kyber
   ActionKyberTradeKovan: "0xF829B506c378AaD11dB7Efe8d626cc7d0e015CBA",
   // ==== Gelato Core ===
-  GelatoCore: "0x7752D2b2B243659251EdD5e85281A1Fa08337F93",
+  GelatoCore: "0xa5A98a6AD379C7B578bD85E35A3eC28AD72A336b",
   // === Conditions ===
   // balance
   ConditionBalance: "0x60621bf3F7132838b27972084eaa56E87395D44B",
