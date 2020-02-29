295fa3c2aa93e717ef7564ce6becfd00f4bab630
Hilmar Orth <hm.orth@aol.com>
2020-02-29 17:46:25 +0000

    Completed cancel script

--- contracts/gelato_core/interfaces/IGelatoCoreTwo.sol
+++ contracts/gelato_core/interfaces/IGelatoCoreTwo.sol
@@ -7,4 +7,16 @@ interface IGelatoCore {
         bytes calldata _conditionPayload,
         bytes calldata _actionPayload
     ) external;
+
+    function cancelExecutionClaim(
+        address[2] calldata _providerAndExecutor,
+        uint256 _executionClaimId,
+        address _userProxy,
+        address[2] calldata _conditionAndAction,
+        bytes calldata _conditionPayload,
+        bytes calldata _actionPayload,
+        uint256 _executionClaimExpiryDate
+    ) external;
+
+    function userByGelatoProxy(address _user) external view returns (address);
 }
