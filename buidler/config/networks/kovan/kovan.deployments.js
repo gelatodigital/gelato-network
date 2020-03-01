4eabe961fcbffff18e17eac8099329a4c87f1cfd
gitpusha <gitpusha@gmail.com>
2020-03-01 03:11:36 +0000

    ScriptGnosisSafeEnableGelatoCoreAndMint deployed

--- buidler/config/networks/kovan/kovan.deployments.js
+++ buidler/config/networks/kovan/kovan.deployments.js
@@ -23,5 +23,6 @@ export const deployments = {
 	// time
 	ConditionTimestampPassed: "0x10A46c633adfe5a6719f3DBd2c162676779fE70B",
 	// ==== Scripts ====
-	ScriptGnosisSafeEnableGelatoCore: "0xf53f625aDE4d53905081cC390845a5f9C2EC137a"
+	ScriptGnosisSafeEnableGelatoCore: "0xf53f625aDE4d53905081cC390845a5f9C2EC137a",
+	ScriptGnosisSafeEnableGelatoCoreAndMint: "0x5993ff30b943dE4c3fDA59d88D87d1661412D101"
 };
