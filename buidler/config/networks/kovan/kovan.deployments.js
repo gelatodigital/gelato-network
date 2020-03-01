aee5589731ce2058d99f646a7da6f8493ff7f0c9
gitpusha <gitpusha@gmail.com>
2020-03-01 06:18:54 +0000

    Major Hack Core is deployed on kovan

--- buidler/config/networks/kovan/kovan.deployments.js
+++ buidler/config/networks/kovan/kovan.deployments.js
@@ -12,7 +12,7 @@ export const deployments = {
 	// kyber
 	ActionKyberTradeKovan: "0xF829B506c378AaD11dB7Efe8d626cc7d0e015CBA",
 	// ==== Gelato Core ===
-	GelatoCore: "0x9af93a6ae59E0783EaE6a4DF217760Fc0f4B6bdA",
+	GelatoCore: "0xf8aC5393609eD48930e86B8648e1BF84DB79af2E",
 	// === Conditions ===
 	// balance
 	ConditionBalance: "0x60621bf3F7132838b27972084eaa56E87395D44B",
