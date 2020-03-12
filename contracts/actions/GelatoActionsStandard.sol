c6f11e6085ea0932d2be306cc7df60733532a968
gitpusha <gitpusha@gmail.com>
2020-03-12 12:38:24 +0100

    MAJOR: ActionStandard has no more actionGas requirement

--- contracts/actions/GelatoActionsStandard.sol
+++ contracts/actions/GelatoActionsStandard.sol
@@ -5,39 +5,6 @@ import "./IGelatoAction.sol";
 /// @title GelatoActionsStandard
 /// @dev find all the NatSpecs inside IGelatoAction
 abstract contract GelatoActionsStandard is IGelatoAction {
-
-    event LogOneWay(
-        address origin,
-        address sendToken,
-        uint256 sendAmount,
-        address destination
-    );
-
-    event LogTwoWay(
-        address origin,
-        address sendToken,
-        uint256 sendAmount,
-        address destination,
-        address receiveToken,
-        uint256 receiveAmount,
-        address receiver
-    );
-
-    /* CAUTION: all actions must have their action() function according to the
-    following standard format:
-        function action(
-            address _user,
-            address _userProxy,
-            address _source,
-            uint256 _sourceAmount,
-            address _destination,
-            ...
-        )
-            external;
-    action function not defined here because non-overridable, due to
-    different arguments passed across different actions
-    */
-
     function actionConditionsCheck(bytes calldata)  // _actionPayload
         external
         view
@@ -49,22 +16,4 @@ abstract contract GelatoActionsStandard is IGelatoAction {
         // Standard return value for actionConditions fulfilled and no erros:
         return "ok";
     }
-
-    /// All actions must override this with their own implementation
-    /*function getUsersSendTokenBalance(
-        address _user,
-        address _userProxy,
-        address _source,
-        uint256 _sourceAmount,
-        address _destination,
-        ...
-    )
-        external
-        view
-        override
-        virtual
-        returns(uint256 userSrcBalance);
-    getUsersSendTokenBalance not defined here because non-overridable, due to
-    different arguments passed across different actions
-    */
 }
