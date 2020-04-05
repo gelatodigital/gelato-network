0044b9cda2b3a58b9e4db491ead8870716be52d2
gitpusha <gitpusha@gmail.com>
2020-04-05 17:28:20 +0200

    Small Buidler task updates

--- buidler/tasks/gelato/core/gelato_providers/collection.tasks.gelato-provider.js
+++ buidler/tasks/gelato/core/gelato_providers/collection.tasks.gelato-provider.js
@@ -8,6 +8,12 @@ import "./provide_funds/task.provideFunds";
 import "./provide_funds/task.unprovideFunds";
 // ProviderExecutor
 import "./task.assignProviderExecutor";
+// Conditions
+import "./provide_conditions/task.provideCondition";
+import "./provide_conditions/task.isProvidedCondition";
+// Actions
+import "./provide_actions/task.provideAction";
+import "./provide_actions/task.isProvidedAction";
 // Provider Module
 import "./task.providerModules";
 
