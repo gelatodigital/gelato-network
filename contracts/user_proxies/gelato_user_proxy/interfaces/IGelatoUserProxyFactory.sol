7d7190b0878eb190121bacab114d58495a599c36
Hilmar Orth <hm.orth@aol.com>
2020-04-23 12:37:23 +0200

    added natspecs for gelato user proxy and factory

--- contracts/user_proxies/gelato_user_proxy/interfaces/IGelatoUserProxyFactory.sol
+++ contracts/user_proxies/gelato_user_proxy/interfaces/IGelatoUserProxyFactory.sol
@@ -8,17 +8,39 @@ interface IGelatoUserProxyFactory {
     event LogCreation(address indexed user, GelatoUserProxy indexed userProxy);
 
     // create
+
+    /// @notice Create gelato user proxy
+    /// @param _optionalMintTasks Optional tasks to mint on gelato
+    /// @param _setupActions Optional actions to execute
+    /// @return userProxy address of deployed proxy contract
     function create(Task[] calldata _optionalMintTasks, Action[] calldata _setupActions)
         external
         payable
         returns(GelatoUserProxy userProxy);
 
     // ______ State Read APIs __________________
+
+    /// @notice Get address of proxy contract from user address (EOA)
+    /// @param _user Address of user (EOA)
+    /// @return Proxy contract address
     function gelatoProxyByUser(address _user) external view returns(GelatoUserProxy);
+
+    /// @notice Get address of user (EOA) from proxy contract address
+    /// @param _proxy Address of proxy contract
+    /// @return User (EOA) address
     function userByGelatoProxy(GelatoUserProxy _proxy) external view returns(address);
 
+    /// @notice Check if proxy was deployed from gelato proxy factory
+    /// @param _proxy Address of proxy contract
+    /// @return true if it was deployed from gelato user proxy factory
     function isGelatoUserProxy(address _proxy) external view returns(bool);
+
+    /// @notice Check if user has deployed a proxy from gelato proxy factory
+    /// @param _user Address of user contract
+    /// @return true if user deployed a proxy from gelato user proxy factory
     function isGelatoProxyUser(address _user) external view returns(bool);
 
+    /// @notice Returns address of gelato
+    /// @return Gelato core address
     function gelatoCore() external pure returns(address);
 }
