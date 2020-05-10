290fc289ef020fb366457be49060d6c638d3be9b
Hilmar Orth <hm.orth@aol.com>
2020-05-10 14:56:14 +0200

    implemented gas price changes

--- contracts/gelato_core/GelatoCore.sol
+++ contracts/gelato_core/GelatoCore.sol
@@ -99,19 +99,19 @@ contract GelatoCore is IGelatoCore, GelatoExecutors {
     }
 
     // ================  CAN EXECUTE EXECUTOR API ============================
-    function canExec(TaskReceipt memory _TR, uint256 _gelatoMaxGas, uint256 _execTxGasPrice)
+    function canExec(TaskReceipt memory _TR, uint256 _gelatoMaxGas, uint256 _gelatoGasPrice)
         public
         view
         override
         returns(string memory)
     {
-        if (_execTxGasPrice != _getGelatoGasPrice()) return "ExecTxGasPriceNotGelatoGasPrice";
+        if (tx.gasprice < _gelatoGasPrice) return "ExecTxGasPriceNotEqualOrGreaterThanGelatoGasPrice";
 
-        if (!isProviderLiquid(_TR.task.provider.addr, _gelatoMaxGas, _execTxGasPrice))
+        if (!isProviderLiquid(_TR.task.provider.addr, _gelatoMaxGas, _gelatoGasPrice))
             return "ProviderIlliquidity";
 
         if (_TR.userProxy != _TR.task.provider.addr) {
-            string memory res = providerCanExec(_TR, _execTxGasPrice);
+            string memory res = providerCanExec(_TR, _gelatoGasPrice);
             if (!res.startsWithOk()) return res;
         }
 
@@ -182,7 +182,7 @@ contract GelatoCore is IGelatoCore, GelatoExecutors {
     enum ExecutionResult { ExecSuccess, CanExecFailed, ExecRevert }
     enum ExecutorPay { Reward, Refund }
 
-    // Execution Entry Point: tx.gasprice must be _getGelatoGasPrice()
+    // Execution Entry Point: tx.gasprice must be greater or equal to _getGelatoGasPrice()
     function exec(TaskReceipt memory _TR) public override {
 
         // Store startGas for gas-consumption based cost and payout calcs
@@ -197,11 +197,12 @@ contract GelatoCore is IGelatoCore, GelatoExecutors {
 
         // memcopy of gelatoMaxGas, to avoid multiple storage reads
         uint256 _gelatoMaxGas = gelatoMaxGas;
+        uint256 _gelatoGasPrice = _getGelatoGasPrice();
 
         ExecutionResult executionResult;
         string memory reason;
 
-        try this.executionWrapper{gas: gasleft() - internalGasRequirement}(_TR, _gelatoMaxGas)
+        try this.executionWrapper{gas: gasleft() - internalGasRequirement}(_TR, _gelatoMaxGas, _gelatoGasPrice)
             returns(ExecutionResult _executionResult, string memory _reason)
         {
             executionResult = _executionResult;
@@ -224,7 +225,7 @@ contract GelatoCore is IGelatoCore, GelatoExecutors {
                 ExecutorPay.Reward,
                 startGas,
                 _gelatoMaxGas,
-                tx.gasprice  // == gelatoGasPrice
+                _gelatoGasPrice
             );
             emit LogExecSuccess(msg.sender, _TR.id, executorSuccessFee, sysAdminSuccessFee);
 
@@ -245,7 +246,7 @@ contract GelatoCore is IGelatoCore, GelatoExecutors {
                     ExecutorPay.Refund,
                     startGas,
                     _gelatoMaxGas,
-                     tx.gasprice  // == gelatoGasPrice
+                     _gelatoGasPrice
                 );
                 emit LogExecReverted(msg.sender, _TR.id, executorRefund, reason);
             }
@@ -253,14 +254,14 @@ contract GelatoCore is IGelatoCore, GelatoExecutors {
     }
 
     // Used by GelatoCore.exec(), to handle Out-Of-Gas from execution gracefully
-    function executionWrapper(TaskReceipt memory taskReceipt, uint256 _gelatoMaxGas)
+    function executionWrapper(TaskReceipt memory taskReceipt, uint256 _gelatoMaxGas, uint256 _gelatoGasPrice)
         public
         returns(ExecutionResult, string memory)
     {
         require(msg.sender == address(this), "GelatoCore.executionWrapper:onlyGelatoCore");
 
         // canExec()
-        string memory canExecRes = canExec(taskReceipt, _gelatoMaxGas, tx.gasprice);
+        string memory canExecRes = canExec(taskReceipt, _gelatoMaxGas, _gelatoGasPrice);
         if (!canExecRes.startsWithOk()) return (ExecutionResult.CanExecFailed, canExecRes);
 
         // Will revert if exec failed => will be caught in exec flow
