d04b1e00ce3991df774e2c41f8f2c8873ef088f4
gitpusha <gitpusha@gmail.com>
2020-06-05 11:22:34 +0200

    MAJOR: introduced Task.selfProviderGasPriceCeil (issue 202)

--- src/classes/gelato/Task.js
+++ src/classes/gelato/Task.js
@@ -1,10 +1,13 @@
 import checkTaskMembers from "../../helpers/gelato/checkTaskMembers";
 
 class Task {
-  constructor({ conditions, actions }) {
+  constructor({ conditions, actions, selfProviderGasPriceCeil }) {
     checkTaskMembers({ conditions, actions });
     this.conditions = conditions ? conditions : [];
     this.actions = actions;
+    this.selfProviderGasPriceCeil = selfProviderGasPriceCeil
+      ? selfProviderGasPriceCeil
+      : 0;
   }
 }
 
