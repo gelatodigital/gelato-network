258952b0639d0513809aa79382ef90ab2a6dc1da
Hilmar X <x@hilmarx.com>
2020-08-04 15:45:24 +0200

    fixed can exec script

--- buidler/tasks/gelato/core/execution/task.canExec.js
+++ buidler/tasks/gelato/core/execution/task.canExec.js
@@ -1,5 +1,6 @@
 import { task, types } from "@nomiclabs/buidler/config";
 import { defaultNetwork } from "../../../../../buidler.config";
+import { ethers } from "ethers";
 
 export default task(
   "gc-canexec",
@@ -22,7 +23,7 @@ export default task(
   .addOptionalParam(
     "fromblock",
     "Search for event logs from block number.",
-    undefined,
+    0,
     types.int
   )
   .addOptionalParam(
@@ -50,21 +51,19 @@ export default task(
       if (!taskArgs.taskreceipt)
         throw new Error("\nUnable to fetch taskReceipt from events");
 
-      const {
-        [taskArgs.executorindex]: gelatoExecutor,
-      } = await ethers.getSigners();
-      const excecutorAddress = await gelatoExecutor.getAddress();
+      const gelatoCore = await run("instantiateContract", {
+        contractname: "GelatoCore",
+        contractaddress: taskArgs.contractaddress,
+        read: true,
+      });
+
+      const excecutorAddress = await gelatoCore.executorByProvider(
+        taskArgs.taskreceipt.provider.addr
+      );
       if (taskArgs.log) {
         console.log(taskArgs);
         console.log(`\n Executor: ${excecutorAddress}\n`);
       }
-
-      const gelatoCore = await run("instantiateContract", {
-        contractname: "GelatoCore",
-        contractaddress: taskArgs.contractaddress,
-        signer: gelatoExecutor,
-        write: true,
-      });
 
       const gelatoMaxGas = await gelatoCore.gelatoMaxGas();
       const gelatoGasPrice = await run("fetchGelatoGasPrice");
@@ -75,7 +74,13 @@ export default task(
           gelatoMaxGas,
           gelatoGasPrice
         );
-        if (taskArgs.log) console.log(`\n Can Exec Result: ${canExecResult}\n`);
+
+        if (taskArgs.log)
+          console.log(
+            `\n Can Exec Result: ${
+              canExecResult === "InvalidExecutor" ? "OK" : canExecResult
+            }\n`
+          );
         return canExecResult;
       } catch (error) {
         console.error(`\n canExec error`, error);
